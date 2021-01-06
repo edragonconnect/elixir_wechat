cabc5d20c7525954ee7a6d7cfd7ca8da76d09dfb
xinz <joy2everyone.z@gmail.com>
2021-01-06 18:05:56 +0800

    Version 0.3.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule WeChat.MixProject do
   def project do
     [
       app: :elixir_wechat,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
